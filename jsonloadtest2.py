4a4df0491f07f8feae6b9ae6d347afeabeec9897
b18c51 <45794151+b18c51@users.noreply.github.com>
2018-12-12 20:43:23 -0600

    parsing json ouput

--- jsonloadtest2.py
+++ jsonloadtest2.py
@@ -4,3 +4,4 @@ json_string = '{"aaaUser":  {"attributes":  {"name":"admin", "pwd":"cisco123"}}}
 parsed_json = json.loads(json_string)
 #print statment is just printing out the key value of name
 print(parsed_json["aaaUser"]['attributes']['name'])
+print(parsed_json["aaaUser"]['attributes']['pwd'])
